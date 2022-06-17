0007fa359d9bfde74927a00245d482a437e991ce
xiuhui <783379078@qq.com>
2022-06-17 13:00:45 +0800

    add test2

---
+++ test2.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+// typedef std::cout cout;
+#define cout std::cout 
+#define endl std::endl 
+// using namespace std::cout;
+// using namespace std::endl;
+int main(){
+    cout<<"111"<<endl;
+    return 0;
+}
